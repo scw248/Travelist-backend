076e9502b65dd36548475e36129682f6abe78a75
github username <github email address>
2019-06-13 11:42:16 -0500

    added foreign keys to db tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_154700) do
+ActiveRecord::Schema.define(version: 2019_06_13_163939) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
