9f9f24545e426e1884a97e09f4bf9ea044ecc61a
github username <github email address>
2019-06-13 10:10:11 -0500

    building private methods for DestinationsController

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,2 +1,15 @@
-class DestinationsController < ApplicationController
+class Api::V1::DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :update, :destroy]
+  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_destination
+      @location = Destination.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def destination_params
+      params.require(:destination).permit(:name, :image, :votes, :price, :description)
+    end
 end
