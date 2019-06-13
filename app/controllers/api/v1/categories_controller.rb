b2bdb844eed7ec1becdb6a16533d2ed089795756
github username <github email address>
2019-06-13 10:56:00 -0500

    built categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,22 @@
-class CategoriesController < ApplicationController
+class Api::V1::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+    # GET /categories
+    def index
+      @categories = Category.all
+  
+      render json: @categories
+    end
+  
+    # GET /categories/1/destinations
+    def show
+      render json: @category 
+      #add status functionality here at some point
+    end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = category.find(params[:id])
+    end
 end
