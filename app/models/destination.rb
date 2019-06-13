8753e6e52089a07d49ae3c8948e2ea2056d7b458
github username <github email address>
2019-06-13 10:57:55 -0500

    updated routes and model associations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,4 @@
 class Destination < ApplicationRecord
-  belongs_to :location
   belongs_to :user
+  has_many :categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :destinations
+  
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :locations
+      resources :categories
+      resources :destinations
     end
   end
 
