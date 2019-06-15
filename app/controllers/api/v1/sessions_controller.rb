1d41c07d2ca2e360ee2495e6ce3c156309ebd313
github username <github email address>
2019-06-15 14:48:50 -0500

    correct sessions controller and added #current_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,22 @@ class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
 
-    if @user && @user.authenticate(params[:sessions][:password])
-      session[user_id] = @user.id
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
       render json: @user
     else
       render json: {
-        error: "Invalid Email/Password"
+        error: 'Invalid Email/Password'
+      }
+    end
+  end
+
+  def get current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: 'Not Logged In'
       }
     end
   end
