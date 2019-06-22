5474f12e0573069ebee66c568c27b43a8827d8a6
github username <github email address>
2019-06-22 12:39:17 -0500

    created and built pinned_destinations controller

---
+++ app/controllers/api/v1/pinned_destinations_controller.rb
@@ -0,0 +1,61 @@
+class Api::V1::DestinationsController < ApplicationController
+  before_action :set_destination, only: [:show, :update, :destroy]
+
+    # GET /users/1/pinned_destinations
+    def index
+
+      if logged_in?
+        @destinations = current_user.destinations
+        render json: DestinationSerializer.new(@destinations)
+      else
+        render json: {
+          error: "You must be logged in to see trips"
+        }
+      end
+    end
+  
+    # GET /users/1/pinned_destinations/1
+    def show
+      render json: @destination 
+      #add status functionality here at some point
+    end
+  
+    # POST /users/1/pinned_destinations
+    def create
+      @destination = current_user.destinations.build(destination_params)
+      if @destination.save
+        render json: DestinationSerializer.new(@destination), status: :created
+      else
+        resp = {
+          error: @destination.errors.full_messages.to_sentence
+        }
+        render json: resp, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /users/1/pinned_destinations/1
+    def update
+      if @destination.update(destination_params)
+        render json: @destination
+      else
+        render json: @destination.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /users/1/pinned_destinations/1
+    def destroy
+      render json: @destination.destroy
+    end
+  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_destination
+      @destination = Destination.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def destination_params
+      params.permit(:name, :image, :price, :description, :city, :state, :country)
+    end
+end
