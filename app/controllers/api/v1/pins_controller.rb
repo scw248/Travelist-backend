e6c788f23c82faa8e72694ea5408aa569a4897c3
github username <github email address>
2019-07-02 14:14:24 -0500

    built pins controller

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,5 +1,34 @@
 class PinsController < ApplicationController
+  before_action :set_destination, only: [:destroy]
 
 
-  
+   # POST /users/1/pinned_destinations
+   def create
+    @pin = current_user.pins.build(destination_id: params[:destination_id])
+    if @pin.save
+      render json: PinSerializer.new(@pin), status: :created
+    else
+      resp = {
+        error: @pin.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1/pinned_destinations/1
+  def destroy
+    render json: @pin.destroy
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pin
+    @pin = Pin.find(params[:destination_id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def pin_params
+    params.permit(:name, :image, :price, :description, :city, :state, :country)
+  end
 end
--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,6 +1,4 @@
-class DestinationSerializer
+class PinSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :votes, :price, :description, :city, :state, :country, :image, :user_id, :destination_id
-  belongs_to :user, serializer: UserSerializer
-  belongs_to :user, serializer: DestinationSerializer
 end
