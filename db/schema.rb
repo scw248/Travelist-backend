221b076f380813fef8285c95763dc010315a546c
github username <github email address>
2019-07-02 14:16:25 -0500

    migrated pin table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_192030) do
+ActiveRecord::Schema.define(version: 2019_07_02_185008) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "destination_id"
@@ -35,6 +35,21 @@ ActiveRecord::Schema.define(version: 2019_06_15_192030) do
     t.binary "image"
     t.index ["category_id"], name: "index_destinations_on_category_id"
     t.index ["user_id"], name: "index_destinations_on_user_id"
+  end
+
+  create_table "pins", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "destination_id"
+    t.string "name"
+    t.integer "votes"
+    t.string "price"
+    t.string "description"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.binary "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
