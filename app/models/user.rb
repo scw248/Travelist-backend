e146512e408c60421b4ed64a01e42d67729eed00
github username <github email address>
2019-07-02 11:59:14 -0500

    updating model associations with pinned_destinations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :destinations
-  # has_many :destinations, through: :pinned_destinations
+  has_many :pinned_destinations, through: :destinations
   validates :email, presence: true
   validates :email, uniqueness: true
 end
--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -3,11 +3,4 @@ class DestinationSerializer
   attributes :name, :votes, :price, :description, :city, :state, :country, :image, :user_id
   has_many :categories, serializer: CategorySerializer
 
-  attribute :categories do |destination|
-    destination.categories.map do |category|
-      {
-        name: category.name
-      }
-    end
-  end
 end
