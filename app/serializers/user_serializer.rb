a225665b30f20866bc683b79ffc12b3d142f65b6
github username <github email address>
2019-07-10 14:28:34 -0500

    added pinned_destinations association with serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email, :password
   has_many :destinations, serializer: DestinationSerializer
+  has_many :pinned_destinations, through: :pins, source: :destination, serializer: DestinationSerializer
 end
