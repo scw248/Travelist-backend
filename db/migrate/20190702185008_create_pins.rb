e3b091492c3f4479119291a8a2e21ae45793780f
github username <github email address>
2019-07-10 14:27:26 -0500

    rolled back migration and corrected with only user/destinations ids

--- db/migrate/20190702185008_create_pins.rb
+++ db/migrate/20190702185008_create_pins.rb
@@ -3,14 +3,6 @@ class CreatePins < ActiveRecord::Migration[5.2]
     create_table :pins do |t|
       t.integer :user_id
       t.integer :destination_id
-      t.string :name
-      t.integer :votes
-      t.string :price
-      t.string :description
-      t.string :city
-      t.string :state
-      t.string :country
-      t.binary :image
 
       t.timestamps
     end
